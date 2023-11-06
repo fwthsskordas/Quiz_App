51be195e543b02a4ce0f0033798eba5649a45e30
fotis <fotis@arx.net>
2023-11-06 17:24:11 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import q from "./data/quizes.json"
+import { ref } from "vue";  
+import { watch } from "vue";
+import Card from "./components/Card.vue"
+
+
+const quizes = ref(q);
+const search = ref(""); 
+
+watch(search, () => {
+  quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
+})
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input v-model.trim="search" type="text">
+    </header>
+    <div class="option-container" placeholder="Search Quiz Here!">
+        <card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
+    </div>
+  </div>
+</template>
+
+
+
+<style scoped>
+ 
+ .container {
+  max-width: 1000px;
+  margin:  0 auto;
+ }
+
+ header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+ }
+
+ header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+ }
+
+ header input {
+  margin-top: 8px;
+  border: none;
+  background-color: rgba(255, 255, 255, 0.76);
+  padding: 10px;
+  border-radius: 10px;
+ }
+.option-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,60 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+
+<script setup>
+import { defineProps } from 'vue'; 
+
+const {quiz} = defineProps(['quiz'])
+</script>
+
+
+<template>
+    <div class="card">
+    <img :src="quiz.img" alt="image">
+    <div class="class-text">
+        <h2> {{ quiz.name }} </h2>
+        <p>{{ quiz.questions.length }} Questions</p>
+    </div>
+    </div>
+</template>
+
+
+
+
+
+<style scoped>
+.card{
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(134, 121, 121, 0.7);
+  margin-right: 20px;
+  cursor: pointer;
+  margin-bottom: 35px;
+}
+
+.card img{
+  width: 100%;
+  height: 190px;
+  margin: 0;
+}
+
+.card .card-text{
+  padding: 0 5px;
+}
+
+.card .card-text h2{
+  font-weight: bold;
+  background-color: beige;
+}
+
+.class-text{
+  background-color: rgb(173, 154, 154);
+}
+
+.class-text h2, p{
+  font-family: 'Courier New', Courier, monospace;
+  color: brown;
+  margin-left: 10px;
+}
+
+</style>
